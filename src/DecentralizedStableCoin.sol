fe8d58699cbb3bf6a49180ce8fb1cd6e1fc75f42
vfr750y <th91843@proton.me>
2025-09-02 12:46:26 +1200

    add DecentralizedStableCoin.sol

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -23,4 +23,60 @@
 
 pragma solidity ^0.8.19;
 
-contract DecentralizedStableCoin {}
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/* @title DecentralizedStableCoin
+ * @author Ajay Curry
+ * Anchored to USD
+ * Stability Mechanism (Minting): Algorithmic (Decentralized)
+ * Collateral: Exongenous (Crypto) - ETH and BTC
+ *
+ * This is the contract meant ot be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
+ */
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__AmountMustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    /*
+    In future versions of OpenZeppelin contracts package, Ownable must be declared with an address of the contract owner
+    as a parameter.
+    For example:
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266) {}
+    Related code changes can be viewed in this commit:
+    https://github.com/OpenZeppelin/openzeppelin-contracts/commit/13d5e0466a9855e9305119ed383e54fc913fdc60
+    */
+    //constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+
+    // In newer versions of OpenZeppelin contracts (e.g., versions compatible with Solidity >=0.8.20), the Ownable contract's constructor requires an initial owner address to be passed explicitly
+    // OpenZeppelin Contracts 5.0.0 and later are compatible with Solidity versions ^0.8.20 (as specified in the package.json and contract source files, which use pragma solidity ^0.8.20).
+    // see Open Zepelin commit : https://github.com/OpenZeppelin/openzeppelin-contracts/commit/13d5e0466a9855e9305119ed383e54fc913fdc60
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}

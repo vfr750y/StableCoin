49a7f9f82f6c62483be489d7f254a8e570ce5c48
vfr750y <th91843@proton.me>
2025-09-03 12:57:54 +1200

    add constructor

--- src/DCSEngine.sol
+++ src/DCSEngine.sol
@@ -22,6 +22,7 @@
 // view & pure functions
 
 pragma solidity ^0.8.19;
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 
 /**
  * * @title DSCEngine
@@ -39,35 +40,71 @@ pragma solidity ^0.8.19;
  * @notice This contract is VERY losely based on the MAkerDAO DSS (DAI) system.
  */
 contract DSCEngine {
-
     /////////////////////////
     //  Errors             //
     /////////////////////////
-error DSCEngine__NeedsMoreThanZero();
-
+    error DSCEngine__NeedsMoreThanZero();
+    error DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+    error DSCEngine__NotAllowedToken();
+    /////////////////////////
+    //  State variables           //
+    /////////////////////////
+    mapping(address => address priceFeed) private s_priceFeeds;
+    DecentralizedStableCoin private immutable i_dsc;
     ////////////////////////////
     //  Modifiers             //
     ////////////////////////////
     modifier moreThanZero(uint256 amount) {
-        if (amount == 0 ){
-            revert DSCEngine__NeedsMoreThanZero();}
+        if (amount == 0) {
+            revert DSCEngine__NeedsMoreThanZero();
         }
+        _;
     }
+
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__NotAllowedToken();
+        }
+        _;
+    }
+
+    //////////////////////////
+    //  Functions           //
+    //////////////////////////
+
+    constructor(
+        address[] memory tokenAddresses,
+        address[] memory priceFeedAddresses,
+        address dscAddress
+    ) {
+        // USD PriceFeeds
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+        }
+        // For example ETH / USD, BTC / USD, MKR / USD etc
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+        i_dsc = DecentralizedStableCoin(dscAddress);
+    }
+
+    ////////////////////////////
+    //  External functions    //
+    ////////////////////////////
+
     function depositCollateralAndMintDsc() external {}
 
-
-/**
- * 
- * @param tokenCollateralAddress The address fo the token to deposit as collateral
- * 
- * @param amountCollateral THe amount of collateral to deposit
- */
-
+    /**
+     *
+     * @param tokenCollateralAddress The address fo the token to deposit as collateral
+     *
+     * @param amountCollateral THe amount of collateral to deposit
+     */
 
     function depositCollateral(
         address tokenCollateralAddress,
         uint256 amountCollateral
-    ) external moreThanZero(amountCollateral){}
+    ) external moreThanZero(amountCollateral) {}
 
     function redeemCollateralForDsc() external {}
 

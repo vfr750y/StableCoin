816ca9c6b9373deef6b8cdc5b8fcc7c916018668
vfr750y <th91843@proton.me>
2025-09-03 12:38:44 +1200

    add modifier

--- src/DCSEngine.sol
+++ src/DCSEngine.sol
@@ -31,12 +31,53 @@ pragma solidity ^0.8.19;
  * * This stablecoin has the properties
  * * - Exogenous Collateral
  *
- * - dollar Pegger
- * Lagorithmically Stable
- * Iti is similare to DAI if DAI had no gevernance, no fees, and was only backed by WETH and WBTC.
+ * - dollar Pegged
+ * Algorithmically Stable
+ * It is similar to DAI if DAI had no gevernance, no fees, and was only backed by WETH and WBTC.
+ * DSC system should always be "overcollateralized"
  * @notice This contract is the core of the DSC SYstem. It handles all the logic for mining and redeeming DSC, as well as depoisiting & withdrawing collateral.
  * @notice This contract is VERY losely based on the MAkerDAO DSS (DAI) system.
  */
 contract DSCEngine {
 
+    /////////////////////////
+    //  Errors             //
+    /////////////////////////
+error DSCEngine__NeedsMoreThanZero();
+
+    ////////////////////////////
+    //  Modifiers             //
+    ////////////////////////////
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0 ){
+            revert DSCEngine__NeedsMoreThanZero();}
+        }
+    }
+    function depositCollateralAndMintDsc() external {}
+
+
+/**
+ * 
+ * @param tokenCollateralAddress The address fo the token to deposit as collateral
+ * 
+ * @param amountCollateral THe amount of collateral to deposit
+ */
+
+
+    function depositCollateral(
+        address tokenCollateralAddress,
+        uint256 amountCollateral
+    ) external moreThanZero(amountCollateral){}
+
+    function redeemCollateralForDsc() external {}
+
+    function redeemCollateral() external {}
+
+    function mintDsc() external {}
+
+    function burnDsc() external {}
+
+    function liquidate() external {}
+
+    function healthFactor() external view {}
 }

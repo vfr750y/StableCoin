6521e5138355c396190dc1bd3f0ee362b2cd49e5
vfr750y <th91843@proton.me>
2025-09-08 11:10:16 +1200

    add test

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -7,6 +7,7 @@ import {DeployDSC} from "../../script/DeployDsc.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -15,6 +16,9 @@ contract DSCEngineTest is Test {
     HelperConfig helperConfig;
     address ethUsdPriceFeed;
     address weth;
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+
+    address public USER = makeAddr("user");
 
     function setUp() public {
         deployer = new DeployDSC();
@@ -30,5 +34,16 @@ contract DSCEngineTest is Test {
         uint256 expectedUsd = 30000e18;
         uint256 actualUsd = dsce.getUsdValue(weth, ethAmount);
         assertEq(expectedUsd, actualUsd);
+    }
+    /////////////////////
+    // PDeposit Collateral TEsts ///////////
+    /////////////////////////
+
+    function testRevertIfCollateralZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
     }
 }

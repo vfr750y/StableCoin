5abf6a93a891ea19b7951f86cb6fc4f2166b8f85
vfr750y <th91843@proton.me>
2025-09-08 11:00:30 +1200

    add test

--- script/DeployDsc.s.sol
+++ script/DeployDsc.s.sol
@@ -11,7 +11,7 @@ contract DeployDSC is Script {
     address[] public tokenAddresses;
     address[] public priceFeedAddresses;
 
-    function run() external returns (DecentralizedStableCoin, DSCEngine) {
+    function run() external returns (DecentralizedStableCoin, DSCEngine, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig();
         (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
             helperConfig.activeNetworkConfig();
@@ -25,6 +25,6 @@ contract DeployDSC is Script {
         dsc.transferOwnership(address(engine));
 
         vm.stopBroadcast();
-        return (dsc, engine);
+        return (dsc, engine, helperConfig);
     }
 }
---
+++ test/unit/DSCEngineTest.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployDSC} from "../../script/DeployDsc.s.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+
+contract DSCEngineTest is Test {
+    DeployDSC deployer;
+    DecentralizedStableCoin dsc;
+    DSCEngine dsce;
+    HelperConfig helperConfig;
+    address ethUsdPriceFeed;
+    address weth;
+
+    function setUp() public {
+        deployer = new DeployDSC();
+        (dsc, dsce, helperConfig) = deployer.run();
+        (ethUsdPriceFeed,, weth,,) = helperConfig.activeNetworkConfig();
+    }
+
+    /////////////////////
+    // Price Test ///////////
+    /////////////////////////
+    function testGetUsdValue() public {
+        uint256 ethAmount = 15e18;
+        uint256 expectedUsd = 30000e18;
+        uint256 actualUsd = dsce.getUsdValue(weth, ethAmount);
+        assertEq(expectedUsd, actualUsd);
+    }
+}

cbf000809dbfaaf6810ddad654e9d5b95ca87916
vfr750y <th91843@proton.me>
2025-09-19 20:23:59 +1200

    add invariants test

---
+++ test/fuzz/InvariantsTest.t.sol
@@ -0,0 +1,26 @@
+//SPDX-License_Identifier:MIT
+
+// This file contains the invariants (properties of our system that should behave the same in all cases)
+// 1. Total supply of DSC should be less than the total collateral
+// 2. The getter view functions should never revert
+
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "./../../script/DeployDsc.s.sol";
+import {DSCEngine} from "./../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "./../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "./../../script/HelperConfig.s.sol";
+
+contract InvariantsTest is StdInvariant, Test {
+    DeployDSC deployer;
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+    HelperConfig config;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, dsce, config) = deployer.run();
+    }
+}

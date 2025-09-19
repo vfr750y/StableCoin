f14e5c557e03e5832355bb956f9bca1881518eaf
vfr750y <th91843@proton.me>
2025-09-19 20:30:15 +1200

    add setup function

--- test/fuzz/OpenInvariantsTest.t.sol
+++ test/fuzz/OpenInvariantsTest.t.sol
@@ -12,15 +12,25 @@ import {DeployDSC} from "./../../script/DeployDsc.s.sol";
 import {DSCEngine} from "./../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "./../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "./../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract OpenInvariantsTest is StdInvariant, Test {
     DeployDSC deployer;
     DSCEngine dsce;
     DecentralizedStableCoin dsc;
     HelperConfig config;
+    address weth;
+    address wbtc;
 
     function setUp() external {
         deployer = new DeployDSC();
         (dsc, dsce, config) = deployer.run();
+        (,, weth, wbtc,) = config.activeNetworkConfig();
+        targetContract(address(dsce));
+    }
+
+    function invariant_protolMustHaceMoreValueTHanTotalSUpply() public view {
+        // get the value of all the collateral in the protocol
+        // comper it a to all the debt (dsc)
     }
 }

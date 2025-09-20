a58843851fe28f39df0834281fb13f7412283f96
vfr750y <th91843@proton.me>
2025-09-20 19:14:20 +1200

    add constructor

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
+import {Test} from "forge-std/Test.sol";
+import {DSCEngine} from "./../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "./../../src/DecentralizedStableCoin.sol";
+
+contract Handler is Test {
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+
+    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc) {
+        dsce = _dscEngine;
+        dsc = _dsc;
+    }
+}
